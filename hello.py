8beefafad14fe79acd059d07100353047dda6784
Daniel J. Kim <daniel_j_kim@ham.honda.com>
2020-02-28 14:00:47 -0500

    updated readme

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 import panel as pn
 
-marquee = pn.panel("<h1><marquee>Hello World!</marquee></h1>", width=500)
+marquee = pn.panel("<h1><marquee>ALL YOUR BASE ARE BELONG TO US!</marquee></h1>", width=500)
 marquee.servable()
